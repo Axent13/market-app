d71ea42f65837836d5edf447bbdc1b7588cba979
Axent13 <axent13@yandex.ru>
2023-08-16 15:00:01 +0700

    build: Настраивает конфиг для разбивки файлов билда по разным папкам

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,29 @@ export default defineConfig({
   server: {
     open: '/',
   },
+  build: {
+    outDir: 'build',
+    assetsDir: 'static',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+            return 'static/img/[name]-[hash][extname]';
+          }
+
+          if (/\.css$/.test(name ?? '')) {
+            return 'static/css/[name]-[hash][extname]';
+          }
+
+          if (/\.woff2?|ttf|eot$/.test(name ?? '')) {
+            return 'static/fonts/[name]-[hash][extname]';
+          }
+
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
 });
